9243b6af6d80f8e2b4bbe9a687c4b57044506290
Miguel Tomairo <matomairo@gmail.com>
2020-04-27 23:40:47 -0500

    Initial Commit

---
+++ testLibrary/testLibrary.h
@@ -0,0 +1,19 @@
+//
+//  testLibrary.h
+//  testLibrary
+//
+//  Created by miguel tomairo on 4/27/20.
+//  Copyright © 2020 rapser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testLibrary.
+FOUNDATION_EXPORT double testLibraryVersionNumber;
+
+//! Project version string for testLibrary.
+FOUNDATION_EXPORT const unsigned char testLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testLibrary/PublicHeader.h>
+
+
