d17406c5e97f182b1077984d68659639bf1bef35
Miguel Tomairo <matomairo@gmail.com>
2020-04-27 23:58:33 -0500

    first commit

---
+++ testLibraryExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testLibraryExamples
+//
+//  Created by miguel tomairo on 4/27/20.
+//  Copyright © 2020 rapser. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
